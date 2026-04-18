fc0dcda3c70557c5f679968c4f18f7c748eff405
Robin Karlsson <robin@example.com>
2026-04-18 19:06:35 +0200

    Flip vertex UV Y-axis so images fill and center in viewport

--- main.js
+++ main.js
@@ -47,7 +47,7 @@ const VERTEX_FALLBACK = `attribute vec2 position;
 attribute vec2 uv;
 varying vec2 vUv;
 void main() {
-    vUv = uv;
+    vUv = vec2(uv.x, 1.0 - uv.y);
     gl_Position = vec4(position, 0.0, 1.0);
 }`;
 
--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -4,6 +4,6 @@ attribute vec2 uv;
 varying vec2 vUv;
 
 void main() {
-    vUv = uv;
+    vUv = vec2(uv.x, 1.0 - uv.y);
     gl_Position = vec4(position, 0.0, 1.0);
 }
